6244bc59f410a6cb323eb211e8cf182a03867017
sarad <DIT220012@spengergasse.at>
2025-03-10 16:00:45 +0100

    Neues Projekt erstellt und mit Datenbank verbunden und KLassen erstellt

---
+++ src/main/java/at/spengergasse/Quiz.java
@@ -0,0 +1,11 @@
+package at.spengergasse;
+
+import at.spengergasse.entities.Answer;
+import at.spengergasse.entities.Question;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Quiz {
+}
---
+++ src/main/java/at/spengergasse/entities/Answer.java
@@ -0,0 +1,61 @@
+package at.spengergasse.entities;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "a_answers")
+
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "a_id")
+    private int id;
+
+    @Column(name = "a_correct")
+    private boolean correct;
+
+    @Column(name = "a_text")
+    private String text;
+
+    public Answer() {
+
+    }
+    public Answer(boolean correct, String text) {
+        this.correct = correct;
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return "Answer{" +
+                "id=" + id +
+                ", correct='" + correct + '\'' +
+                ", text='" + text + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean getCorrect() {
+        return correct;
+    }
+
+    public void setCorrect(boolean correct) {
+        this.correct = correct;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
